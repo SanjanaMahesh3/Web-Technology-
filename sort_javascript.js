0c769e66d28c99f6428dab8de37a15eecd7413b5
SanjanaMahesh3 <76697358+SanjanaMahesh3@users.noreply.github.com>
2021-12-25 18:15:11 +0530

    Create sort_javascript.js

---
+++ sort_javascript.js
@@ -0,0 +1,8 @@
+var numbers = [4, 2, 5, 1, 3];
+var numbers1 = [4, 2, 5, 1, 3];
+numbers.sort(function(a, b) { return b
+- a; });
+document.write(numbers);
+numbers1.sort()
+document.write("<br>")
+document.write(numbers1);
